68849ca678975412663345f2ffe723e14c21bddb
miquel.montfort <miquel.montfort@auleseebe.upc.edu>
2024-09-19 13:41:19 +0200

    ...

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,16 @@
 #Codigo para problema 3
+x <- c(-8, 5, 2, -8, 9, 5, 2, -3, 1, -1, 4, -4, 9, 3, -9, 7, 0, -7, 8, -4, 1, 7, -6, 4, 5, -9, -2, -8, 5, -5) #c() concatenacion
+x
+
+x <- (1:30)/30
+sum(x)
+
+x[2] #extrae la posicion de un vector
+x[c(2, 4,6)]
+x[-c(2, 4, 6)] #quito la posicion especificada
+
+y <- x[-c(21, 22, 26)]
+y
+sum(exp(x)) - sum(exp(y))
+    
+    
